fa6811869e51f1e8b595a66a93ab663984613049
bahti41 <116912093+bahti41@users.noreply.github.com>
2023-12-12 22:59:30 +0300

    c# giriş

---
+++ Section-06-TemelProgramlama/Week-09/12-12-2023/P01-HelloWord/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Section-06-TemelProgramlama/Week-09/12-12-2023/P03-Types/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P03_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DataTypes
+            //C#'ta veri tipleri çok önemlidir. Çeşitli veri tipleri bulunmaktadır. Özellikle sayılar ve sayı olmayanlar şeklinde incelemek iyi olacaktır.
+            //TAM SAYILAR
+            byte sayi1 = 255;
+            sbyte sayi2 = 15;
+            Console.WriteLine(sayi1 + " sayısının alabileceği minimum değer: " + byte.MinValue);
+            Console.WriteLine(sayi1 + " sayısının alabileceği maksimum değer: " + byte.MaxValue);
+            Console.WriteLine(sayi1 + " sayısının bellekte kapladığı alan: " + sizeof(byte) + " byte");
+            Console.WriteLine(sayi2 + "-" + sizeof(double) + " byte");
+
+            #endregion
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
