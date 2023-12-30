3fa394c231dc87f2fa32d0c2df368a0a765c4183
bati <116912093+bahti41@users.noreply.github.com>
2023-12-30 18:56:36 +0300

    düzenleme

---
+++ Section-09-MVC/Week-11/28-12-2023/Proj06_MvcWivcWithSql/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Proj06_MvcWivcWithSql.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
---
+++ Section-09-MVC/Week-11/28-12-2023/Proj06_MvcWivcWithSql/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proj06_MvcWivcWithSql.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
