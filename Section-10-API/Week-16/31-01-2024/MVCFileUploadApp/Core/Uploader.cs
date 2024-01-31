6d9d2d5989c699552f717a7bf6b56d38473b6559
bati <116912093+bahti41@users.noreply.github.com>
2024-01-31 22:04:51 +0300

    imga yükleme

---
+++ Section-10-API/Week-16/31-01-2024/MVCFileUploadApp/Core/Uploader.cs
@@ -0,0 +1,33 @@
+﻿namespace MVCFileUploadApp.Core
+{
+    public static class Uploader
+    {
+        public static string UploadImage(IFormFile image)
+        {
+            //dosyaismi.uzantı
+            //manzara.kadikoy.png = [manzara,kadikoy,png]
+            //Örnek image ismi: iphone12.png
+
+
+            //.png
+            var extension = "." + image.FileName.Split('.')[image.FileName.Split('.').Length - 1];
+            
+            //localhost:5000/wwwroot/images
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","images");
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            //aksdahjflkasdhldskafjhladksfhjladskfjhasdklfsadj.png
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            //localhost:5000/wwwroot/images/aksdahjflkasdhldskafjhladksfhjladskfjhasdklfsadj.png
+            var path = Path.Combine(filePath, fileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            };
+            return fileName;
+        }
+    }
+}
