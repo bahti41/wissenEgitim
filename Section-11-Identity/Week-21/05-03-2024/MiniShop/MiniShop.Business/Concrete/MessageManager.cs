1e7473bb694ba2f4248e41ce220ac2ea7534411c
bati <116912093+bahti41@users.noreply.github.com>
2024-03-05 21:26:36 +0300

    messageGönderme

---
+++ Section-11-Identity/Week-21/05-03-2024/MiniShop/MiniShop.Business/Concrete/MessageManager.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using MiniShop.Business.Abstract;
+using MiniShop.Data.Abstract;
+using MiniShop.Entity.Concrete;
+using MiniShop.Shared.ResponseViewModels;
+using MiniShop.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Business.Concrete
+{
+    public class MessageManager : IMessagaService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMessageRepository _repository;
+
+        public MessageManager(IMapper mapper, IMessageRepository messageRepository)
+        {
+            _mapper = mapper;
+            _repository = messageRepository;
+        }
+
+        public async Task<Response<MessageDetailsViewModel>> CreateAsync(MessageDetailsViewModel messageDetailsViewModel)
+        {
+            var message = _mapper.Map<Message>(messageDetailsViewModel);
+            var createdMessage = await _repository.CreateAsync(message);
+            if (createdMessage == null)
+            {
+                return Response<MessageDetailsViewModel>.Fail("Kategori oluşturulamadı");
+            }
+            var createdMessageViewModel = _mapper.Map<MessageDetailsViewModel>(createdMessage);
+            return Response<MessageDetailsViewModel>.Success(createdMessageViewModel);
+        }
+
+        public Task<Response<List<MessageDetailsViewModel>>> GetAllAsync(string userId, bool isRead = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<MessageDetailsViewModel>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<int>> GetMessageCount(string userId, bool isRead = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoContent>> HardDeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Section-11-Identity/Week-21/05-03-2024/MiniShop/MiniShop.UI/Extensions/HostServiceExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniShop.Data.Concrete.Contexts;
+
+namespace MiniShop.UI.Extensions
+{
+    public static class HostServiceExtension
+    {
+        public static IHost UpdateDataBase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using(var miniShopDbContext = scope.ServiceProvider.GetRequiredService<MiniShopDbContext>())
+                {
+                    try
+                    {
+                        var pendingMigrations = miniShopDbContext.Database.GetPendingMigrations().Count();
+                        if(pendingMigrations > 0) miniShopDbContext.Database.Migrate();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
